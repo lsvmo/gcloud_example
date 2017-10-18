9280c56a95f1322458bf0e34a2cc1939641c4628
Лукьянов Сергей <s.lukyanov@Securitycode.ru>
2017-10-18 01:38:33 +0100

    new import 2

--- bot_gae.py
+++ bot_gae.py
@@ -4,7 +4,7 @@ import sys
 import os
 sys.path.append(os.path.join(os.path.abspath('.'), 'venv/lib/site-packages'))
 
-from flask.ext import telegram
+import telegram
 from flask import Flask, request
 
 app = Flask(__name__)
