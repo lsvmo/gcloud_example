fa1a87bae6a00c0088fe94f646ab05c0e65333b6
Лукьянов Сергей <s.lukyanov@Securitycode.ru>
2017-10-19 14:21:21 +0100

    Fix: intendation

--- bot_gae.py
+++ bot_gae.py
@@ -15,36 +15,35 @@ bot = telegram.Bot(token='291279465:AAGRvWtOS2VjhfIH_l-d5tn2EdXdTFZheo4')
 
 @app.route('/HOOK', methods=['POST'])
 def webhook_handler():
-    if request.method == "POST":
-        # retrieve the message in JSON and then transform it to Telegram object
-        update = telegram.Update.de_json(request.get_json(force=True), bot)
-        
-        chat_id = update.message.chat.id
-
-        # Telegram understands UTF-8, so encode text for unicode compatibility
-        text = update.message.text.encode('utf-8')
-        
-        #First row - Month and Year
-        row=[]
-        row.append(telegram.InlineKeyboardButton(text,callback_data="ignore"))
+	if request.method == "POST":
+		# retrieve the message in JSON and then transform it to Telegram object
+		update = telegram.Update.de_json(request.get_json(force=True), bot)
+		
+		chat_id = update.message.chat.id
+		
+		# Telegram understands UTF-8, so encode text for unicode compatibility
+		text = update.message.text.encode('utf-8')
+		
+		#First row - Month and Year
+		row=[]
+		row.append(telegram.InlineKeyboardButton(text,callback_data="ignore"))
 		markup = telegram.InlineKeyboardMarkup(row)
 		markup.row(*row)
-        
-        # repeat the same message back (echo)
-        bot.sendMessage(chat_id=chat_id, text="Choose smth!", reply_markup=markup)
-
+		
+		# repeat the same message back (echo)
+		bot.sendMessage(chat_id=chat_id, text="Choose smth!", reply_markup=markup)
     return 'ok'
 
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = bot.setWebhook('https://botgamereg-hello.appspot.com/HOOK')
-    if s:
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
+	s = bot.setWebhook('https://botgamereg-hello.appspot.com/HOOK')
+	if s:
+		return "webhook setup ok"
+	else:
+		return "webhook setup failed"
 
 
 @app.route('/')
 def index():
-    return '.'
+	return '.'
