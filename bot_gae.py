9a446c096f40c4cf037b82205d4f68c30184d524
Лукьянов Сергей <s.lukyanov@Securitycode.ru>
2017-10-19 17:11:49 +0100

    Add: ReplyKeyboardMarkup

--- bot_gae.py
+++ bot_gae.py
@@ -25,9 +25,9 @@ def webhook_handler():
 		msg_text = update.message.text.encode('utf-8')
 		
 		#First row - Month and Year
-		row=[[telegram.InlineKeyboardButton(msg_text,callback_data="ignore")]]
+		row=[[telegram.KeyboardButton(msg_text,one_time_keyboard=True)]]
 		#row.append(telegram.InlineKeyboardButton(msg_text,callback_data="ignore"))
-		markup = telegram.InlineKeyboardMarkup(row)
+		markup = telegram.ReplyKeyboardMarkup(row)
 		
 		# repeat the same message back (echo)
 		bot.sendMessage(chat_id=chat_id, text=msg_text, reply_markup=markup)
