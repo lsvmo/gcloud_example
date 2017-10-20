668621c9532e1c8f7038dfd4251083f2b65725a0
Sergey <s.lukyanov@securitycode.ru>
2017-10-20 19:12:39 +0300

    Starting development

--- bot_gae.py
+++ bot_gae.py
@@ -6,29 +6,37 @@ sys.path.append(os.path.join(os.path.abspath('.'), 'lib'))
 
 import telegram
 from flask import Flask, request
+from peewee import *
+
+
+# --------- global constant section --------------
+
+
+# --------- global variables ---------------------
 
 app = Flask(__name__)
 
 global bot
 bot = telegram.Bot(token='291279465:AAGRvWtOS2VjhfIH_l-d5tn2EdXdTFZheo4')
 
+# --------- functions ----------------------------
 
 @app.route('/HOOK', methods=['POST'])
 def webhook_handler():
 	if request.method == "POST":
 		# retrieve the message in JSON and then transform it to Telegram object
 		update = telegram.Update.de_json(request.get_json(force=True), bot)
-		
+
 		chat_id = update.message.chat.id
-		
+
 		# Telegram understands UTF-8, so encode text for unicode compatibility
 		msg_text = update.message.text.encode('utf-8')
-		
+
 		#First row - Month and Year
 		row=[[telegram.KeyboardButton(msg_text)]]
 		#row.append(telegram.InlineKeyboardButton(msg_text,callback_data="ignore"))
 		markup = telegram.ReplyKeyboardMarkup(row, resize_keyboard=True, one_time_keyboard=True)
-		
+
 		# repeat the same message back (echo)
 		bot.sendMessage(chat_id=chat_id, text=msg_text, reply_markup=markup)
 	return 'ok'
